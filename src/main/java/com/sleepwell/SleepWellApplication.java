280a8950f516efade6efdf685bee8911bfc77d64
rio <75190035+HyoJongPark@users.noreply.github.com>
2024-04-30 13:34:57 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/sleepwell/SleepWellApplication.java
@@ -0,0 +1,13 @@
+package com.sleepwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepWellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepWellApplication.class, args);
+	}
+
+}
