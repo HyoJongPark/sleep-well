866cddd01026ee843626312ca32ec1dbdf39b658
rio <75190035+HyoJongPark@users.noreply.github.com>
2024-05-03 13:10:04 +0900

    feat: 사용자 엔티티 구현

---
+++ src/main/java/com/sleepwell/user/domain/SocialType.java
@@ -0,0 +1,5 @@
+package com.sleepwell.user.domain;
+
+public enum SocialType {
+	GOOGLE
+}
---
+++ src/main/java/com/sleepwell/user/domain/User.java
@@ -0,0 +1,44 @@
+package com.sleepwell.user.domain;
+
+import com.sleepwell.common.domain.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+	private static final String DEFAULT_IMAGE_URL = "";
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	private String socialId;
+
+	@Column
+	@Enumerated(EnumType.STRING)
+	private SocialType socialType;
+
+	@Column
+	private String nickname;
+
+	@Column
+	private String profileImage = DEFAULT_IMAGE_URL;
+
+	public User(String socialId, SocialType socialType, String nickname, String profileImage) {
+		this.socialId = socialId;
+		this.socialType = socialType;
+		this.nickname = nickname;
+		this.profileImage = profileImage;
+	}
+}
