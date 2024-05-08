ff3d465b2547f81c58cdf435d901f48652a5f0ed
rio <75190035+HyoJongPark@users.noreply.github.com>
2024-05-08 14:56:30 +0900

    feat: Interceptor를 통해 admin/normal api에 대한 사용자에 권한을 검사하도록 변경

--- src/main/java/com/sleepwell/auth/utils/JwtInterceptor.java
+++ src/main/java/com/sleepwell/auth/utils/JwtInterceptor.java
@@ -1,21 +1,29 @@
 package com.sleepwell.auth.utils;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.sleepwell.auth.dto.Principle;
+import com.sleepwell.user.domain.Role;
 
 import jakarta.annotation.Nullable;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 
-@Component
-@RequiredArgsConstructor
 public class JwtInterceptor implements HandlerInterceptor {
 
+	private final Role role;
+
+	@Autowired
 	private final JwtExtractor jwtExtractor;
+	@Autowired
 	private final AuthenticationStorage authenticationStorage;
+
+	public JwtInterceptor(Role role, JwtExtractor jwtExtractor, AuthenticationStorage authenticationStorage) {
+		this.role = role;
+		this.jwtExtractor = jwtExtractor;
+		this.authenticationStorage = authenticationStorage;
+	}
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
--- src/main/java/com/sleepwell/config/WebConfig.java
+++ src/main/java/com/sleepwell/config/WebConfig.java
@@ -2,13 +2,17 @@ package com.sleepwell.config;
 
 import java.util.List;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.sleepwell.auth.utils.AuthUserResolver;
+import com.sleepwell.auth.utils.AuthenticationStorage;
+import com.sleepwell.auth.utils.JwtExtractor;
 import com.sleepwell.auth.utils.JwtInterceptor;
+import com.sleepwell.user.domain.Role;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,18 +20,32 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-	private final JwtInterceptor jwtInterceptor;
+	private final JwtExtractor jwtExtractor;
 	private final AuthUserResolver authUserResolver;
+	private final AuthenticationStorage authenticationStorage;
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(jwtInterceptor)
+		registry.addInterceptor(normalJwtInterceptor())
 			.excludePathPatterns("/auth/**", "/error", "/*.html")
-			.addPathPatterns("/**");
+			.addPathPatterns("/rooms/**", "/coupon/**", "/reservation/**", "/user/**");
+
+		registry.addInterceptor(adminJwtInterceptor())
+			.addPathPatterns("/admin/**");
 	}
 
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(authUserResolver);
+	}
+
+	@Bean
+	public JwtInterceptor normalJwtInterceptor() {
+		return new JwtInterceptor(Role.NORMAL, jwtExtractor, authenticationStorage);
+	}
+
+	@Bean
+	public JwtInterceptor adminJwtInterceptor() {
+		return new JwtInterceptor(Role.ADMIN, jwtExtractor, authenticationStorage);
 	}
 }
