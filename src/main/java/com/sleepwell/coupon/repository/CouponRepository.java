8dde17db9856456c69b37068029c2c06dde26493
rio <75190035+HyoJongPark@users.noreply.github.com>
2024-05-19 19:00:03 +0900

    feat: synchronized 동기화로 방식 수정

--- src/main/java/com/sleepwell/coupon/repository/CouponRepository.java
+++ src/main/java/com/sleepwell/coupon/repository/CouponRepository.java
@@ -3,13 +3,10 @@ package com.sleepwell.coupon.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 
 import com.sleepwell.coupon.domain.Coupon;
 
-import jakarta.persistence.LockModeType;
-
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
-	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	// @Lock(LockModeType.PESSIMISTIC_WRITE)
 	Optional<Coupon> findByCouponCode(String couponCode);
 }
