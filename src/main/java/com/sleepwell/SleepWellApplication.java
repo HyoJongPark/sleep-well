47cec8c39d45101df0ffe60ff47e00854a1dca07
rio <75190035+HyoJongPark@users.noreply.github.com>
2024-05-20 19:37:06 +0900

    fix: createdAt, updatedAt이 기록되지 않는 문제 수정

--- src/main/java/com/sleepwell/SleepWellApplication.java
+++ src/main/java/com/sleepwell/SleepWellApplication.java
@@ -2,12 +2,13 @@ package com.sleepwell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SleepWellApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SleepWellApplication.class, args);
 	}
-
 }
